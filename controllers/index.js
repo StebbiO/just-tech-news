238e909e5ed4e18fccbb2c589ed8eee25e06a234
Stebbi <stefodd@yahoo.com>
2022-05-04 18:42:47 -0500

    login/logout show conditionally, single page upvote, comment

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 
 const { route } = require('./api');
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
-
+router.use('/', homeRoutes);
 router.use((req, res) => {
     res.status(404).end();
 });
