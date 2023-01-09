b73b36089841d1fbdc1b04b8dc64f40f94e6466e
Stebbi <stefodd@yahoo.com>
2023-01-09 17:18:54 -0600

    add stylesheet

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connections');
 
+const path = require('path');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
