28bde75dff9bf198e8d45cac278a2ad88f8c780c
Stebbi <stefodd@yahoo.com>
2023-01-09 17:51:20 -0600

    setup view template via handlebars

--- config/connections.js
+++ config/connections.js
@@ -1,13 +1,18 @@
 // import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
+let Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our database, pass in MySQL info for username and pw
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+if(process.env.JAWSBD_URL) {
+    sequelize = new Sequelize(process.env.JAWSBD_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD ,{
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
