6cee3af7ed765acae8295cc339a695c72a002ed4
Stebbi <stefodd@yahoo.com>
2023-01-12 17:37:25 -0600

    homepage template setup

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connections');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const sequelize = require('./config/connections');
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
